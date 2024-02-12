98037ec5758b5799965e9b46b6c6e8141f39b8f7
ridurahman <lindaridu@gmail.com>
2024-02-12 23:40:43 +0600

    Updated

--- mergeTwoBinaryTrees.js
+++ mergeTwoBinaryTrees.js
@@ -17,19 +17,18 @@ const makeTree = (values, i = 0) => {
 };
 
 function mergeTree(root1, root2) {
-  if (root1 == null) {
-    return root2;
-  } else if (root2 == null) {
+    if (root1 == null) {
+      return root2;
+    } else if (root2 == null) {
+      return root1;
+    }
+    //console.log(root1.value, root2.value);
+    root1.value += root2.value;
+  
+    root1.left = mergeTree(root1.left, root2.left);
+    root1.right = mergeTree(root1.right, root2.right);
     return root1;
   }
-  //console.log(root1.value, root2.value);
-  root1.value += root2.value;
-
-  root1.left = mergeTree(root1.left, root2.left);
-  root1.right = mergeTree(root1.right, root2.right);
-  return root1;
-}
-
 let root1 = [1, 3, 2, 5];
 let root2 = [2, 1, 3, null, 4, null, 7];
 
@@ -61,5 +60,5 @@ bfsTraversal(rootNode2);
 let root3 = mergeTree(rootNode1, rootNode2);
 bfsTraversal(root3);
 
-// Time Complexity: mergeTree() has O(n) time complexity.
-// Space Complexity: 
+// Time Complexity: mergeTree() has O(n+m) time complexity where n is the length of root1 array and m is the length of root2 array.
+// Space Complexity: mergeTree() has O(n+m) space complexity where n is the length of root1 array and m is the length of root2 array.
